c8c1ba5e5911b3e614dbc4b5d8af08d196ef7e28
nom <nom@nom.org>
2021-07-17 03:22:01 +0800

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-deploy";
+
+let { TESTNET, MAINNET, BSCSCAN_API_KEY } = require("./secret");
+
+module.exports = {
+  solidity: "0.8.6",
+  networks: {
+    testnet: {
+      url: TESTNET.ENDPOINT,
+      accounts: [TESTNET.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: MAINNET.ENDPOINT,
+      accounts: [MAINNET.PRIVATE_KEY],
+    },
+  },
+
+  etherscan: {
+    apiKey: BSCSCAN_API_KEY,
+  },
+
+  namedAccounts: {
+    deployer: 0,
+  },
+};
