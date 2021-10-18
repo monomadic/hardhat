e42adfeaa73683b6822a99d86d51cea179bc1fe9
Monomadic <deathdisco@protonmail.com>
2021-10-18 11:58:32 +0800

    version bump

--- hardhat/contracts/Token.sol
+++ hardhat/contracts/Token.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: UNLICENSE
-pragma solidity 0.8.4;
+// Token.sol
+
+pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PmknToken is ERC20, Ownable {
+import "hardhat/console.sol";
 
-    constructor() ERC20("TokenName", "TOK") {}
+contract Token is ERC20, Ownable {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
